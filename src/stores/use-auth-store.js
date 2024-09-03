9d72641f9d0a5443daccf27cc8a489a4ad655c5b
MoonLoveless <lexyls523@gmail.com>
2024-09-03 12:02:15 -0500

    corrigiendo ruta de firebase

--- src/stores/use-auth-store.js
+++ src/stores/use-auth-store.js
@@ -1,37 +1,40 @@
 import { create } from "zustand";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../../firebase.config";
-
+import { auth } from "../firebase.config"; // Ruta corregida
 
 const provider = new GoogleAuthProvider();
+
 const useAuthStore = create((set) => ({
     user: null,
     loading: true,
     
     loginGoogleWithPopup: async () => {
+        set({ loading: true });
         try {
-             await signInWithPopup(auth, provider);
-            //set({ user: result.user, loading: false });
+            const result = await signInWithPopup(auth, provider);
+            set({ user: result.user, loading: false });
         } catch (error) {
             console.error(error);
+            set({ loading: false });
         }
     },
 
     logout: async () => {
+        set({ loading: true });
         try {
             await auth.signOut();
             set({ user: null, loading: false });
         } catch (error) {
             console.error(error);
+            set({ loading: false });
         }
     },
-
 
     observeAuthState: () => {
         auth.onAuthStateChanged((user) => {
             set({ user, loading: false });
         });
     },
-    }));
+}));
 
-    export default useAuthStore;
+export default useAuthStore;
