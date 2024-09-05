4123dd19f3789470bddeef993087034867dee649
MoonLoveless <lexyls523@gmail.com>
2024-09-04 21:25:46 -0500

    corrigiendo errores

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,16 +1,17 @@
+import React, { useEffect, useCallback } from "react";
 import useQuizStore from "../../stores/use-quiz-store";
-import React, { useEffect, useCallback } from 'react';
 
 export default function Quiz() {
   const { quiz, aumentarPorcentaje } = useQuizStore();
 
+  // Asegúrate de que se quite el fondo de login si llegas aquí
   useEffect(() => {
-    document.body.classList.remove("login-background");
+    document.body.classList.remove('login-background');
   }, []);
 
   const onHandlerButtonNext = useCallback(() => {
     aumentarPorcentaje();
-  });
+  }, [aumentarPorcentaje]);
 
   return (
     <div>
