e0ee3e68bbee01f523cc7a09d723d81a74152e1c
MoonLoveless <lexyls523@gmail.com>
2024-11-15 00:17:12 -0500

    Haciendo controles de teclado

--- src/controls/CameraDeforestation.jsx
+++ src/controls/CameraDeforestation.jsx
@@ -1,28 +1,52 @@
-import { useRef, useState } from "react";
+import { useRef, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 
 const CameraDeforestation = ({ targetPosition, targetLookAt }) => {
-    const controlsRef = useRef();
-    const [cameraPosition, setCameraPosition] = useState(new THREE.Vector3(-14.203040677019562, 3.5904266270657557, 60.276562580416254));
-    const [cameraTarget, setCameraTarget] = useState(new THREE.Vector3(-8.744576996657683, -3.0750795814342533, 1.3205744708974023));
+  const controlsRef = useRef();
+  const cameraPosition = useRef(targetPosition.clone());
+  const cameraTarget = useRef(targetLookAt.clone());
 
-    useFrame(() => {
-        cameraPosition.lerp(targetPosition, 0.05);
-        setCameraPosition(cameraPosition.clone());
-    
-        cameraTarget.lerp(targetLookAt, 0.05);
-        setCameraTarget(cameraTarget.clone()); 
-    
-        if (controlsRef.current) {
-            controlsRef.current.object.position.copy(cameraPosition);
-            controlsRef.current.target.copy(cameraTarget);
-            controlsRef.current.update();
-        }
-    });
+  const handleKeyDown = (event) => {
+    switch (event.code) {
+      case "ArrowUp":
+        // Mover la cámara a un punto predefinido
+        targetPosition.set(98.45, 2.85, 133.23);
+        targetLookAt.set(25.69, -15.25, 150.73);
+        break;
+      case "ArrowDown":
+        // Mover la cámara de vuelta a la posición inicial
+        targetPosition.set(-14.2, 3.59, 60.27);
+        targetLookAt.set(-8.74, -3.07, 1.32);
+        break;
+      default:
+        break;
+    }
+  };
 
-    return <OrbitControls ref={controlsRef} />;
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  useFrame(() => {
+    // Interpolación suave hacia el objetivo
+    cameraPosition.current.lerp(targetPosition, 0.05);
+    cameraTarget.current.lerp(targetLookAt, 0.05);
+
+    // Actualizar OrbitControls
+    if (controlsRef.current) {
+      controlsRef.current.object.position.copy(cameraPosition.current);
+      controlsRef.current.target.copy(cameraTarget.current);
+      controlsRef.current.update();
+    }
+  });
+
+  return <OrbitControls ref={controlsRef} />;
 };
 
 export default CameraDeforestation;
