fd371121b90749570c2cb7f3701d19c14868bffb
MoonLoveless <lexyls523@gmail.com>
2024-11-26 01:49:03 -0500

    haciendo boton html 3D para la pagina de la deforestacion

--- src/components/LostDeforestation.jsx
+++ src/components/LostDeforestation.jsx
@@ -49,6 +49,30 @@ const LostDeforestation = (props) => {
               scale={[115.932, 169.274, 174.736]}
             />
           </RigidBody>
+
+          <RigidBody type="fixed" colliders="trimesh">
+  <group
+    name="stump_2005"
+    position={[14.408, -3.454, 169.232]}
+    rotation={[-Math.PI, 0.312, -Math.PI]}
+    scale={1.644}
+  >
+    <mesh
+      name="Plane006"
+      castShadow
+      receiveShadow
+      geometry={nodes.Plane006.geometry}
+      material={materials['Material.002']}
+    />
+    <mesh
+      name="Plane006_1"
+      castShadow
+      receiveShadow
+      geometry={nodes.Plane006_1.geometry}
+      material={materials['Material.004']}
+    />
+  </group>
+</RigidBody>
   
           <TreeRigidBody
             treeName="Sphere005"
@@ -94,31 +118,7 @@ const LostDeforestation = (props) => {
                 material={materials['leaves.001']}
               />
             </group>
-          </TreeRigidBody>
-  
-          <TreeRigidBody
-            treeName="Sphere025"
-            position={[6.574, -0.317, 149.039]}
-            scale={[0.871, 10.988, 0.871]}
-          >
-            <group name="Sphere025">
-              <mesh
-                name="Sphere031"
-                castShadow
-                receiveShadow
-                geometry={nodes.Sphere031.geometry}
-                material={materials['wood.001']}
-              />
-              <mesh
-                name="Sphere031_1"
-                castShadow
-                receiveShadow
-                geometry={nodes.Sphere031_1.geometry}
-                material={materials['leaves.001']}
-              />
-            </group>
-          </TreeRigidBody>
-  
+          </TreeRigidBody>  
           <TreeRigidBody
             treeName="Sphere017"
             position={[-16.792, 4.165, 175.988]}
@@ -142,195 +142,88 @@ const LostDeforestation = (props) => {
               />
             </group>
           </TreeRigidBody>
-  
-          {/* Otros objetos de la escena sin físicas */}
+
+          <group
+          name="stump_2004"
+          position={[2.523, -1.56, 78.002]}
+          rotation={[-Math.PI, 0.312, -Math.PI]}
+          scale={1.644}>
           <mesh
-            name="Log001"
+            name="Plane004"
             castShadow
             receiveShadow
-            geometry={nodes.Log001.geometry}
+            geometry={nodes.Plane004.geometry}
             material={materials['Material.002']}
-            position={[68.353, -1.67, 64.214]}
-            rotation={[0, 0.932, 0]}
-            scale={1.644}
           />
-  
-          <group name="Sphere030" position={[89.439, -2.323, 98.52]} scale={[3.609, 1.86, 3.609]}>
-            <mesh
-              name="Sphere027"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027.geometry}
-              material={materials.leaves}
-            />
-            <mesh
-              name="Sphere027_1"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027_1.geometry}
-              material={materials.wood}
-            />
-            <mesh
-              name="Sphere027_2"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027_2.geometry}
-              material={materials.Rock}
-            />
-            <mesh
-              name="Sphere027_3"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027_3.geometry}
-              material={materials.Grass}
-            />
-            <mesh
-              name="Sphere027_4"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027_4.geometry}
-              material={materials.Flowers03}
-            />
-            <mesh
-              name="Sphere027_5"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027_5.geometry}
-              material={materials.Flowers01}
-            />
-            <mesh
-              name="Sphere027_6"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027_6.geometry}
-              material={materials['Material.002']}
-            />
-            <mesh
-              name="Sphere027_7"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027_7.geometry}
-              material={materials['Material.004']}
-            />
-            <mesh
-              name="Sphere027_8"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027_8.geometry}
-              material={materials['wood.001']}
-            />
-            <mesh
-              name="Sphere027_9"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere027_9.geometry}
-              material={materials['leaves.001']}
-            />
-          </group>
-  
-          <group name="Sphere024" position={[39.59, 1.799, 166.35]} scale={[0.871, 10.988, 0.871]}>
-            <mesh
-              name="Sphere021"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere021.geometry}
-              material={materials['wood.001']}
-            />
-            <mesh
-              name="Sphere021_1"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere021_1.geometry}
-              material={materials['leaves.001']}
-            />
-          </group>
-  
-          <group name="Sphere011" position={[34.77, 2.055, 103.743]} rotation={[0, 1.021, 0]} scale={[0.871, 10.988, 0.871]}>
-            <mesh
-              name="Sphere025_1"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere025_1.geometry}
-              material={materials['wood.001']}
-            />
-            <mesh
-              name="Sphere025_2"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere025_2.geometry}
-              material={materials['leaves.001']}
-            />
-          </group>
-  
-          <group name="Sphere034" position={[59.733, 1.481, 142.429]} scale={[0.871, 10.988, 0.871]}>
-            <mesh
-              name="Sphere034_1"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere034_1.geometry}
-              material={materials['wood.001']}
-            />
-            <mesh
-              name="Sphere034_2"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere034_2.geometry}
-              material={materials['leaves.001']}
-            />
-          </group>
-  
-          <group name="Sphere035" position={[49.503, 1.729, 196.706]} rotation={[0, 1.021, 0]} scale={[0.871, 10.988, 0.871]}>
-            <mesh
-              name="Sphere035_1"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere035_1.geometry}
-              material={materials['wood.001']}
-            />
-            <mesh
-              name="Sphere035_2"
-              castShadow
-              receiveShadow
-              geometry={nodes.Sphere035_2.geometry}
-              material={materials['leaves.001']}
-            />
-          </group>
-  
-          <group name="stump_2008" position={[76.871, -1.819, 131.247]} rotation={[-Math.PI, 0.312, -Math.PI]} scale={2.041}>
-            <mesh
-              name="Plane013"
-              castShadow
-              receiveShadow
-              geometry={nodes.Plane013.geometry}
-              material={materials['Material.002']}
-            />
-            <mesh
-              name="Plane013_1"
-              castShadow
-              receiveShadow
-              geometry={nodes.Plane013_1.geometry}
-              material={materials['Material.004']}
-            />
-          </group>
-  
-          <group name="stump_2010" position={[76.473, -3.297, 165.718]} rotation={[-Math.PI, 0.312, -Math.PI]} scale={1.644}>
-            <mesh
-              name="Plane015"
-              castShadow
-              receiveShadow
-              geometry={nodes.Plane015.geometry}
-              material={materials['Material.002']}
-            />
-            <mesh
-              name="Plane015_1"
-              castShadow
-              receiveShadow
-              geometry={nodes.Plane015_1.geometry}
-              material={materials['Material.004']}
-            />
-          </group>
+          <mesh
+            name="Plane004_1"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_1.geometry}
+            material={materials['Material.004']}
+          />
+          <mesh
+            name="Plane004_2"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_2.geometry}
+            material={materials.Grass}
+          />
+          <mesh
+            name="Plane004_3"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_3.geometry}
+            material={materials.Rock}
+          />
+          <mesh
+            name="Plane004_4"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_4.geometry}
+            material={materials.wood}
+          />
+          <mesh
+            name="Plane004_5"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_5.geometry}
+            material={materials.Flowers03}
+          />
+          <mesh
+            name="Plane004_6"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_6.geometry}
+            material={materials.Flowers01}
+          />
+          <mesh
+            name="Plane004_7"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_7.geometry}
+            material={materials.leaves}
+          />
+          <mesh
+            name="Plane004_8"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_8.geometry}
+            material={materials['wood.001']}
+          />
+          <mesh
+            name="Plane004_9"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_9.geometry}
+            material={materials['leaves.001']}
+          />
         </group>
-      </group>
+        </group>
+    </group>
+
+          
+          
     );
   };
   
--- src/pages/enviromental_impacts/Deforestation.jsx
+++ src/pages/enviromental_impacts/Deforestation.jsx
@@ -1,108 +1,86 @@
-// src/pages/Deforestation.jsx
-import React, { Suspense, useState, useCallback, useEffect, useRef, useMemo } from "react";
+import React, { Suspense, useState, useCallback, useEffect, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Stars, Sky, OrbitControls } from "@react-three/drei";
-import Navbar from "../../components/Navbar";
+import { Stars, Sky, Html } from "@react-three/drei";
 import { useNavigate } from "react-router-dom";
 import * as THREE from "three";
+import { Physics } from '@react-three/rapier';
 import CameraDeforestation from "../../controls/CameraDeforestation";
 import DeforestationTitle from "../../components/DeforestationTitle";
 import CustomCursor from "../../controls/CustomCursor";
 import LightsDeforestation from "../../components/LightsDeforestation";
 import LostDeforestation from "../../components/LostDeforestation";
-import './styles.css'; // Importar la hoja de estilos
-import { Physics } from '@react-three/rapier';
 import Cat from "../../components/CatModel";
+import './styles.css';
 
-// Definición de posiciones de la cámara y textos fuera del componente para evitar recrearlos en cada render
-const cameraPositions = [
-  // Paso 0: Posición inicial (Título)
+// Camera positions and steps defined as constants
+const CAMERA_POSITIONS = [
   {
     position: new THREE.Vector3(-14.2, 3.59, 60.27),
     lookAt: new THREE.Vector3(-8.74, -3.07, 1.32),
   },
-  // Paso 1: La Deforestación
   {
     position: new THREE.Vector3(98.45, 2.85, 133.23),
     lookAt: new THREE.Vector3(25.69, -15.25, 150.73),
   },
-  // Paso 2: La Sensibilización
   {
-    position: new THREE.Vector3(77.34154334345368,-2.6468275771793213, 220.49287339767233),
-    lookAt: new THREE.Vector3(-5.723287859275314,-12.093047405869553,-12.732195097274296),
+    position: new THREE.Vector3(77.34154334345368, -2.6468275771793213, 220.49287339767233),
+    lookAt: new THREE.Vector3(-5.723287859275314, -12.093047405869553, -12.732195097274296),
   },
-  // Paso 3: Soluciones a la Deforestación
   {
     position: new THREE.Vector3(115.65618342706782, 4.300917195859684, 81.6113805558299),
     lookAt: new THREE.Vector3(93.38266673088074, -5.518344414133743, 56.07500598020611),
   },
-  // Paso 4: ¡Juega y Aprende!
   {
     position: new THREE.Vector3(52.53859280193333, 3.15010345942333, 111.65783993430324),
     lookAt: new THREE.Vector3(30.947823498215268, -4.468295888068112, 136.13396587114272),
   },
 ];
 
-const texts = [
-  // Paso 0: Posición inicial (Título)
+const STEPS = [
   {
     title: "Bienvenido a la Exploración de la Deforestación",
     content: "Haz clic en la pantalla para comenzar y aprender más sobre la deforestación. También puedes usar las flechas izquierda y derecha del teclado para desplazarte, y la tecla escape para volver a comenzar. ¡Diviértete!",
-    isClickable: true, // Indica que se puede hacer clic para avanzar
+    isClickable: true,
   },
-  // Paso 1: La Deforestación
   {
     title: "La Deforestación",
-    content:
-      "La deforestación amenaza la biodiversidad y acelera el cambio climático al destruir hábitats vitales y reducir los pulmones verdes de nuestro planeta. Cada árbol talado significa menos oxígeno, mayor emisión de carbono y pérdida de especies únicas. Es crucial adoptar prácticas sostenibles y proteger nuestros bosques para asegurar un futuro saludable para las próximas generaciones. Actuar ahora es responsabilidad de todos.",
+    content: "La deforestación amenaza la biodiversidad y acelera el cambio climático al destruir hábitats vitales y reducir los pulmones verdes de nuestro planeta. Cada árbol talado significa menos oxígeno, mayor emisión de carbono y pérdida de especies únicas. Es crucial adoptar prácticas sostenibles y proteger nuestros bosques para asegurar un futuro saludable para las próximas generaciones. Actuar ahora es responsabilidad de todos.",
   },
-  // Paso 2: La Sensibilización
   {
     title: "La Sensibilización",
-    content:
-      "La sensibilización es clave para combatir la deforestación. Al educar y concienciar a las comunidades sobre el valor de los bosques y los efectos devastadores de su pérdida, fomentamos acciones responsables y sostenibles. Participa en iniciativas locales, apoya prácticas ecológicas y comparte información para inspirar a otros a proteger nuestro planeta.",
+    content: "La sensibilización es clave para combatir la deforestación. Al educar y concienciar a las comunidades sobre el valor de los bosques y los efectos devastadores de su pérdida, fomentamos acciones responsables y sostenibles. Participa en iniciativas locales, apoya prácticas ecológicas y comparte información para inspirar a otros a proteger nuestro planeta.",
   },
-  // Paso 3: Soluciones a la Deforestación
   {
     title: "Soluciones a la Deforestación",
-    content:
-      "Existen múltiples soluciones para enfrentar la deforestación, tales como la reforestación, la implementación de prácticas agrícolas sostenibles, la creación de reservas naturales y la promoción de políticas ambientales estrictas. La tecnología también juega un papel crucial mediante el monitoreo satelital y el uso de drones para detectar actividades ilegales. La colaboración internacional y el apoyo a las comunidades locales son esenciales para el éxito de estas iniciativas.",
+    content: "Existen múltiples soluciones para enfrentar la deforestación, tales como la reforestación, la implementación de prácticas agrícolas sostenibles, la creación de reservas naturales y la promoción de políticas ambientales estrictas. La tecnología también juega un papel crucial mediante el monitoreo satelital y el uso de drones para detectar actividades ilegales. La colaboración internacional y el apoyo a las comunidades locales son esenciales para el éxito de estas iniciativas.",
   },
-  // Paso 4: ¡Juega y Aprende!
   {
     title: "¡Juega y Aprende!",
-    content:
-      "Participa en nuestro juego interactivo para aprender más sobre la deforestación y cómo puedes contribuir a la preservación de los bosques. Desafía tus conocimientos, toma decisiones estratégicas y descubre cómo tus acciones pueden marcar la diferencia. ¡Diviértete mientras haces del mundo un lugar mejor!",
-    isGame: true, // Indicador para manejar la navegación al juego
+    content: "Participa en nuestro juego interactivo para aprender más sobre la deforestación y cómo puedes contribuir a la preservación de los bosques. Desafía tus conocimientos, toma decisiones estratégicas y descubre cómo tus acciones pueden marcar la diferencia. ¡Diviértete mientras haces del mundo un lugar mejor!",
+    isGame: true,
   },
 ];
 
 const Deforestation = () => {
-  const [targetPosition, setTargetPosition] = useState(cameraPositions[0].position.clone());
-  const [targetLookAt, setTargetLookAt] = useState(cameraPositions[0].lookAt.clone());
+  const [targetPosition, setTargetPosition] = useState(CAMERA_POSITIONS[0].position.clone());
+  const [targetLookAt, setTargetLookAt] = useState(CAMERA_POSITIONS[0].lookAt.clone());
   const [showInfoCanvas, setShowInfoCanvas] = useState(false);
   const [currentStep, setCurrentStep] = useState(0);
   const navigate = useNavigate();
   const controlsRef = useRef();
+  const modalRef = useRef(null);
+  const isFirstRender = useRef(true);
+  const [isButtonPressed, setIsButtonPressed] = useState(false);
 
-  const modalRef = useRef(null); // Referencia al modal
-  const isFirstRender = useRef(true); // Indicador de primera carga
-
-  /**
-   * Función para avanzar al siguiente paso.
-   */
   const nextStep = useCallback(() => {
     setCurrentStep((prevStep) => {
-      if (prevStep < texts.length - 1) {
+      if (prevStep < STEPS.length - 1) {
         return prevStep + 1;
       }
       return prevStep;
     });
   }, []);
 
-  /**
-   * Función para retroceder al paso anterior.
-   */
   const prevStep = useCallback(() => {
     setCurrentStep((prevStep) => {
       if (prevStep > 0) {
@@ -112,48 +90,34 @@ const Deforestation = () => {
     });
   }, []);
 
-  /**
-   * Función para reiniciar la cámara a su posición inicial y ocultar el canvas de información.
-   */
   const resetCamera = useCallback(() => {
-    setTargetPosition(cameraPositions[0].position.clone());
-    setTargetLookAt(cameraPositions[0].lookAt.clone());
+    setTargetPosition(CAMERA_POSITIONS[0].position.clone());
+    setTargetLookAt(CAMERA_POSITIONS[0].lookAt.clone());
     setShowInfoCanvas(false);
     setCurrentStep(0);
     console.log("Cámara reiniciada a la posición inicial.");
   }, []);
 
-  /**
-   * useEffect para sincronizar la posición de la cámara con el paso actual.
-   */
   useEffect(() => {
     console.log(`currentStep cambiado a: ${currentStep}`);
 
     if (isFirstRender.current) {
-      // En la primera renderización, no mostrar el modal
       isFirstRender.current = false;
       setShowInfoCanvas(false);
-      // En el paso 0, targetPosition ya está establecido en posición0
     } else {
-      if (currentStep < cameraPositions.length) {
-        const { position, lookAt } = cameraPositions[currentStep];
+      if (currentStep < CAMERA_POSITIONS.length) {
+        const { position, lookAt } = CAMERA_POSITIONS[currentStep];
         setTargetPosition(position.clone());
         setTargetLookAt(lookAt.clone());
-
-        // Mostrar el modal para todos los pasos, excepto el paso 0
         setShowInfoCanvas(currentStep !== 0);
       }
     }
   }, [currentStep]);
 
-  /**
-   * Función para manejar eventos de teclado.
-   * @param {KeyboardEvent} event - El evento de teclado.
-   */
   const handleKeyDown = useCallback((event) => {
     console.log(`Tecla presionada: ${event.code}, showInfoCanvas: ${showInfoCanvas}, currentStep: ${currentStep}`);
 
-    if (showInfoCanvas) { // Si el modal está abierto
+    if (showInfoCanvas) {
       switch (event.code) {
         case "ArrowRight":
           console.log("ArrowRight presionada");
@@ -170,89 +134,77 @@ const Deforestation = () => {
         default:
           break;
       }
-    } else { // Si el modal no está abierto
+    } else {
       if (event.code === "ArrowRight") {
         console.log("ArrowRight presionada - avanzar al siguiente paso");
         nextStep();
-        setShowInfoCanvas(true); // Mostrar el modal al avanzar
+        setShowInfoCanvas(true);
       }
-      // Puedes agregar otras acciones si es necesario
     }
   }, [showInfoCanvas, nextStep, prevStep, resetCamera, currentStep]);
 
-  /**
-   * useEffect para añadir y limpiar el listener global de keydown.
-   */
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
-
-    // Limpia el listener al desmontar el componente
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
   }, [handleKeyDown]);
 
-  /**
-   * useEffect para enfocar el modal cuando se abre.
-   */
   useEffect(() => {
     if (showInfoCanvas && modalRef.current) {
-      // Añadir un pequeño retraso para asegurar que el modal esté montado
       setTimeout(() => {
         modalRef.current.focus();
       }, 0);
     }
   }, [showInfoCanvas]);
 
-  // Memoizar los valores de la cámara para evitar recreaciones innecesarias
-  const cameraProps = useMemo(() => ({
-    position: cameraPositions[0].position.toArray(),
+  const cameraProps = {
+    position: CAMERA_POSITIONS[0].position.toArray(),
     fov: 75,
-  }), []);
+  };
 
-  const handleCameraChange = () => {
-    // Obtiene el objetivo actual de la cámara (target) cuando OrbitControls cambia
-    if (controlsRef.current) {
-      console.log(controlsRef);
-    }
+  const handleButtonPress = () => {
+    setIsButtonPressed(true);
+    setTimeout(() => setIsButtonPressed(false), 150);
   };
 
   return (
     <div className="deforestation-container">
-      {/* Componente Navbar */}
-      <Navbar />
-
-      {/* Botón para navegar a la página de Biodiversidad */}
-      <button
-        onClick={() => navigate("/biodiversity")}
-        className="navigate-button"
-      >
-        Explora la Biodiversidad
-      </button>
-
-      {/* Canvas 3D para la escena de deforestación */}
       <Canvas
         frameloop="demand"
         shadows
         camera={cameraProps}
         style={{ background: "transparent" }}
       >
-        {/* <OrbitControls ref={controlsRef} onChange={handleCameraChange} /> */}
         <CameraDeforestation
           targetPosition={targetPosition}
           targetLookAt={targetLookAt}
           nextStep={nextStep}
           prevStep={prevStep}
           resetCamera={resetCamera}
-          isModalOpen={showInfoCanvas} // Pasar la prop aquí
+          isModalOpen={showInfoCanvas}
         />
         <Suspense fallback={null}>
           <Physics gravity={[0, -9.81, 0]} defaultContactMaterial={{ restitution: 0.2, friction: 1 }}>
             <LostDeforestation />
             <Cat position={[30, -13, 13]} />
-            {/* <Debug color="black" scale={1.1}> */} {/* Descomenta esto para visualizar los cuerpos de física */}
           </Physics>
-          
+          <Html fullscreen style={{ pointerEvents: 'none' }}>
+            <div className="button-container">
+              <button
+                onClick={() => {
+                  handleButtonPress();
+                  navigate("/biodiversity");
+                }}
+                onMouseDown={handleButtonPress}
+                onMouseUp={() => setIsButtonPressed(false)}
+                onMouseLeave={() => setIsButtonPressed(false)}
+                className={`navigate-button-3d ${isButtonPressed ? 'pressed' : ''}`}
+              >
+                Explora la Biodiversidad
+              </button>
+            </div>
+          </Html>
           <DeforestationTitle />
           <Sky
             sunPosition={[100, 20, 100]}
@@ -274,33 +226,29 @@ const Deforestation = () => {
         <LightsDeforestation />
       </Canvas>
 
-      {/* Clickable overlay para el Paso 0 */}
       {currentStep === 0 && (
         <div
           onClick={nextStep}
           className="modal-paso0"
         >
           <h2 className="modal-title">
-            {texts[currentStep].title}
+            {STEPS[currentStep].title}
           </h2>
           <p className="modal-content">
-            {texts[currentStep].content}
+            {STEPS[currentStep].content}
           </p>
         </div>
       )}
 
-      {/* Canvas de información para la deforestación (Pasos 1 a 4) */}
-      {showInfoCanvas && currentStep >= 1 && currentStep < texts.length && (
+      {showInfoCanvas && currentStep >= 1 && currentStep < STEPS.length && (
         <div
           ref={modalRef}
-          tabIndex={0} // Hace que el div sea enfocables
+          tabIndex={0}
           className="info-modal"
         >
-          {/* Botón de cierre para el modal */}
           <button
             onClick={() => {
               setShowInfoCanvas(false);
-              // No reiniciar currentStep para permitir la navegación con los botones
             }}
             className="close-button"
             aria-label="Cerrar"
@@ -308,17 +256,14 @@ const Deforestation = () => {
             ×
           </button>
 
-          {/* Contenido dinámico basado en el paso actual */}
           <h2 className="modal-title">
-            {texts[currentStep].title}
+            {STEPS[currentStep].title}
           </h2>
           <p className="modal-content">
-            {texts[currentStep].content}
+            {STEPS[currentStep].content}
           </p>
 
-          {/* Botones de navegación */}
           <div className="navigation-buttons">
-            {/* Botón Anterior */}
             {currentStep > 0 && (
               <button
                 onClick={prevStep}
@@ -328,8 +273,7 @@ const Deforestation = () => {
               </button>
             )}
 
-            {/* Botón Siguiente o Finalizar */}
-            {currentStep < texts.length - 1 ? (
+            {currentStep < STEPS.length - 1 ? (
               <button
                 onClick={nextStep}
                 className="nav-button next-button"
@@ -337,7 +281,6 @@ const Deforestation = () => {
                 Siguiente
               </button>
             ) : (
-              // Botón Finalizar que navega al juego
               <button
                 onClick={() => navigate("/juego")}
                 className="nav-button finish-button"
@@ -346,7 +289,6 @@ const Deforestation = () => {
               </button>
             )}
 
-            {/* Botón Resetear Cámara */}
             <button
               onClick={resetCamera}
               className="nav-button reset-button"
@@ -361,3 +303,4 @@ const Deforestation = () => {
 };
 
 export default Deforestation;
+
